59d21607574e87bb5d6671ecf96e70c153729df2
Siqing Mu <siqingmu@gmail.com>
2020-05-30 13:11:25 +0800

    Initial Commit

---
+++ BMITime/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BMITime
+//
+//  Created by Siqing Mu on 5/30/20.
+//  Copyright © 2020 khow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
